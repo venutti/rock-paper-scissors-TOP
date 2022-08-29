68af02d2468d9534ecaa4ec52e59b0ddc5ace721
venutti <lvenutti@fi.uba.ar>
2022-08-29 16:08:55 -0300

    Implemento funcionalidad básica
    Se crean las funciones para obtener las elecciones del jugador y la computadora, y para actuar en consecuencia.

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log("Funca!!")
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return computerChoice;
+}
+
+function getPlayerChoice() {
+    const playerChoice = prompt("What do you choose? Rock, paper or scissors?");
+    return capitalize(playerChoice);
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a tie. The computer choose ${computerSelection} too.`
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Paper" || 
+    playerSelection === "Paper" && computerSelection === "Scissors" || 
+    playerSelection === "Scissors" && computerSelection === "Rock") {
+        return `JA! You lose. ${computerSelection} beats ${playerSelection}.`;
+    }
+    else {
+        return `Lucky! ${playerSelection} beats ${computerSelection}.`
+    }
+}
+
+console.log(getPlayerChoice());
