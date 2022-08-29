01a0f16e6be2b7ade333208712c41c9f53358e24
venutti <lvenutti@fi.uba.ar>
2022-08-29 16:28:00 -0300

    Implemento el juego
    Para poder separar mejor las tareas, creé las funciones "getMessage...".

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function capitalize(string) {
+function normalizeString(string) {
     return string[0].toUpperCase() + string.slice(1);
 }
 
@@ -10,21 +10,56 @@ function getComputerChoice() {
 
 function getPlayerChoice() {
     const playerChoice = prompt("What do you choose? Rock, paper or scissors?");
-    return capitalize(playerChoice);
+    return normalizeString(playerChoice);
+}
+
+function getMessageRound(playerSelection, computerSelection, roundResult) {
+    if (roundResult === 1) {
+        return `Lucky! ${playerSelection} beats ${computerSelection}.`;
+    } else if (roundResult === -1) {
+        return `JA! You lose. ${computerSelection} beats ${playerSelection}.`;
+    } else {
+        return `It's a tie. The computer choose ${computerSelection} too.`;
+    }
+}
+
+function getMessageGame(totalRounds, score) {
+    if (score > totalRounds / 2) {
+        return `You are the winner!!`;
+    } else {
+        return `Better luck next time, loser.`;
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return `It's a tie. The computer choose ${computerSelection} too.`
+        return 0
     }
     else if (playerSelection === "Rock" && computerSelection === "Paper" || 
     playerSelection === "Paper" && computerSelection === "Scissors" || 
     playerSelection === "Scissors" && computerSelection === "Rock") {
-        return `JA! You lose. ${computerSelection} beats ${playerSelection}.`;
+        return -1;
     }
     else {
-        return `Lucky! ${playerSelection} beats ${computerSelection}.`
+        return 1;
     }
 }
 
-console.log(getPlayerChoice());
+function game() {
+    const totalRounds = 5;
+    let score = 0;
+
+    for (let i = 0; i < totalRounds; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection, computerSelection);
+        if (roundResult === 1) {
+            score++;
+        }
+        console.log(getMessageRound(playerSelection, computerSelection, roundResult));
+    }
+    
+    console.log(getMessageGame(totalRounds, score));
+}
+
+game();
